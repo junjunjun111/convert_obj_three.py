3740fac14f5fcc22561a0b25a6a6afa7f400b2d5
Yanlin Ma <ym1399@nyu.edu>
2017-07-05 21:08:50 -0700

    3

---
+++ ground.js
@@ -0,0 +1,33 @@
+{
+    "metadata" :
+    {
+        "formatVersion" : 3.1,
+        "sourceFile"    : "ground.obj",
+        "generatedBy"   : "OBJConverter",
+        "vertices"      : 4,
+        "faces"         : 1,
+        "normals"       : 1,
+        "colors"        : 0,
+        "uvs"           : 0,
+        "materials"     : 1
+    },
+    "scale" : 1.000000,
+    "materials": [	{
+	"DbgColor" : 15658734,
+	"DbgIndex" : 0,
+	"DbgName" : "None",
+	"colorAmbient" : [0.0, 0.0, 0.0],
+	"colorDiffuse" : [0.8, 0.8, 0.8],
+	"colorSpecular" : [0.8, 0.8, 0.8],
+	"illumination" : 2,
+	"specularCoef" : 0.0,
+	"transparency" : 1.0
+	}],
+    "vertices": [-2.939518,0.303050,4.102954,5.212116,0.303050,4.102954,-2.939518,0.303050,-4.048680,5.212116,0.303050,-4.048680],
+    "morphTargets": [],
+    "morphColors": [],
+    "normals": [0,1,0],
+    "colors": [],
+    "uvs": [[]],
+    "faces": [35,0,1,3,2,0,0,0,0,0]
+}
